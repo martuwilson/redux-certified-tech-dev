334d708bbeb325b8c9b2d5f1defe13a7aa7ef269
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-13 16:08:25 -0300

    Contador component agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' */
 import './App.css'
+import Contador from './components/Contador'
 
 function App() {
 
   return (
     <div>
       <h1>Redux Toolkit</h1>
+      <Contador />
     </div>
   )
 }
