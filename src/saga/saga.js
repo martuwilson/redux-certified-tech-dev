c77ccbae4f33adab93d5688c931c0e12cc9a6854
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-13 19:18:39 -0300

    Redux Saga --- implementacion inicial

---
+++ src/saga/saga.js
@@ -0,0 +1,35 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+import { failedFetchPokemons, fetchPokemons, loadingPokemons } from "../reducers/pokemonSlice";
+
+// Servicio para traer los datos de la API de pokemon
+const fetchPokemon = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+    const data = response.json();
+
+    return data;
+}
+
+// Saga para obtener los datos de la API de pokemon
+function* getPokemons(action) {
+    const page = action.payload;
+
+    try {
+        yield put(loadingPokemons());
+
+        //obtener datos de la api
+        const data = yield call(fetchPokemon, page)
+
+        //guardar datos en la store
+        yield put(fetchPokemons({pokemons: data.results, offset: page}));
+
+    } catch (error) {
+        yield put(failedFetchPokemons({error: error.message}));
+    }
+}
+
+// saga principal para observar la accion de getPokemons
+function* watchGetPokemons() {
+    yield takeEvery('pokemon/fetchPokemons', getPokemons) // del 1er param --> pokemon es el name del slice y fetchPokemons es el nombre de la accion
+}
+
+export default watchGetPokemons;
